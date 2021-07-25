921e9a15a80b723a6644aba5a362bbea73f115b5
Jang-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-07-25 12:33:01 +0900

    commit auto 2021-07-25.md push

--- boj_11723/main.cpp
+++ boj_11723/main.cpp
@@ -13,8 +13,9 @@ class Command
 {
 private:
     vector <int> V_list;
-    std::vector<int>::iterator it;
+
 public:
+    std::vector<int>::iterator it;
     void add(int number);
     void remove(int number);
     void check(int number);
