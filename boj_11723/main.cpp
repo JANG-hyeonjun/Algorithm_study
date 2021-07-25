76297766dc933b18fe59b1b37cc3f6485b7bfe57
Jang-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-07-25 11:03:00 +0900

    commit auto 2021-07-25.md push

--- boj_11723/main.cpp
+++ boj_11723/main.cpp
@@ -3,11 +3,20 @@
 
 using namespace std;
 
+int * command_num = new int;
 
+
+class Command
+{
+private:
+    vector <int> V_list;
+public:
+    void add(int )
+};
 
 int main() {
     ios_base::sync_with_stdio(0);
-    
+    cin >> *command_num;
 
 
 
