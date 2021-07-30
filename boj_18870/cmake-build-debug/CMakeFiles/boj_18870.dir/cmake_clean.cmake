6599fb1e5cb583ac92c79b0fceee4416d8cc86b5
Jang-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-07-30 22:29:24 +0900

    commit auto 2021-07-30.md push

---
+++ boj_18870/cmake-build-debug/CMakeFiles/boj_18870.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boj_18870.dir/main.cpp.o"
+  "boj_18870"
+  "boj_18870.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boj_18870.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
