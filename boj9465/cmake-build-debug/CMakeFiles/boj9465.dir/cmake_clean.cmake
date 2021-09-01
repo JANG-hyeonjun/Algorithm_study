807c30c0a6348a742ce58f741fefa0aec9a27b2e
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-09-02 00:47:17 +0900

    solve

---
+++ boj9465/cmake-build-debug/CMakeFiles/boj9465.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boj9465.dir/main.cpp.o"
+  "boj9465"
+  "boj9465.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boj9465.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
