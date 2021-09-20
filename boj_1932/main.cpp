0cd90ba07170a501c31b9462439212e70bc8d143
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-09-20 17:57:28 +0900

    commit auto 2021-09-20.md push

--- boj_1932/main.cpp
+++ boj_1932/main.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 using namespace std;
 
-const int MAX = 3000;
+const int MAX = 502;
 int arr[MAX][MAX];
 int dp[MAX][MAX];
 int result;
@@ -11,10 +11,13 @@ void dp_function()
 {
     for(int i = 1; i < number; i++)
     {
-        for(int j = 0; j < i * 2; j++)
+        for(int j = 0; j <= i; j++)
         {
-            dp[i][j] = 9;
-            //logic 짜면 됨 
+            //dp[i][j] = 9;
+            //logic 짜면 됨
+            if(j == 0) arr[i][j] += arr[i-1][j];
+            else if(j == i) arr[i][j] += arr[i-1][j-1];
+            else arr[i][j] += max(arr[i-1][j],arr[i-1][j-1]);
         }
     }
 }
@@ -33,7 +36,7 @@ int main() {
             cin >> arr[i][j];
         }
     }
-    dp[0][0] = arr[0][0];
+    //dp[0][0] = arr[0][0];
     /*
     cout << '\n';
     for(int i =0; i < number + 2; i++)
@@ -52,10 +55,16 @@ int main() {
     {
         for(int j =0; j < number + 5; j++)
         {
-            cout << dp[i][j];
+            cout << arr[i][j] << " ";
         }
         cout << '\n';
     }
      */
+    int Max = -1;
+    for(int i = 0; i < number; i++)
+    {
+        Max = max(Max,arr[number-1][i]);
+    }
+    cout << Max << '\n';
     return 0;
 }
