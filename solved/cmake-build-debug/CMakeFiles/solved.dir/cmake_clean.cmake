aa1027f7ef2b8dcf37d61a9cf453bb6c1dc4791f
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-10-22 23:07:35 +0900

    commit auto 2021-10-22.md push

---
+++ solved/cmake-build-debug/CMakeFiles/solved.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solved.dir/main.cpp.o"
+  "solved"
+  "solved.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solved.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
