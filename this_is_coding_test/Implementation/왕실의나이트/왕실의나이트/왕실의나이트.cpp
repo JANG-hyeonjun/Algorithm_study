4de1132c0bf39db9cef65675467894fc229216f4
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-06-01 21:51:44 +0900

    push

--- this_is_coding_test/Implementation/왕실의나이트/왕실의나이트/왕실의나이트.cpp
+++ this_is_coding_test/Implementation/왕실의나이트/왕실의나이트/왕실의나이트.cpp
@@ -2,19 +2,57 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <string>
+
+class KindomOfNight
+{
+public:
+    KindomOfNight(std::string);
+    KindomOfNight* FIndAnswerCase();
+    void  PrintAnswer();
+
+private:
+    std::vector<std::pair<int, int>> direction;
+    int mRow;
+    int mCol;
+    int answer;
+};
+
+KindomOfNight::KindomOfNight(std::string position)
+{
+    direction = { {-2,-1},{-2,1},{2,-1},{2,1},{1,2},{-1,2},{1,-2}, {-1,-2} };
+    mRow = position[0] - 'a' + 1;
+    mCol = position[1] - '0';
+}
+
+KindomOfNight* KindomOfNight::FIndAnswerCase()
+{
+    for (size_t idx = 0; idx < 8; ++idx)
+    {
+        int nextRow = mRow + direction[idx].first;
+        int nextCol = mCol + direction[idx].second;
+        
+        if (nextRow <= 8 && nextRow >= 1 && nextCol <= 8 && nextCol >= 1)
+        {
+            answer++;
+        }
+    }
+    return this;
+}
+void KindomOfNight::PrintAnswer()
+{ 
+    std::cout << answer << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string pos;
+    std::cin >> pos;
+
+    KindomOfNight* kindomOfNight = new KindomOfNight(pos);
+    kindomOfNight->FIndAnswerCase()->PrintAnswer();
+
+    return 0;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
