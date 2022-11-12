fd4ce078a153f29826b7711da21173adb42cf1c1
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-11-12 12:42:01 +0900

    solved Algorithm

---
+++ 백준/boj_2805/boj_2805/boj_2805.cpp
@@ -0,0 +1,61 @@
+﻿// boj_2805.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+int N, M;
+std::vector<int> treeHeight;
+
+long long cutTree(int cutHeightValue)
+{
+	long long returnVal = 0;
+	
+	for(int idx = 0; idx < treeHeight.size(); ++idx)
+	{
+		if (cutHeightValue < treeHeight[idx])
+			returnVal += std::abs(cutHeightValue - treeHeight[idx]);
+	}
+	return returnVal;
+}
+
+void solution()
+{
+	int answer = 0;
+
+	int start = 1;
+	int end = 2000000001;
+
+	while (start <= end)
+	{
+		int mid = (start + end) / 2;
+		long long getTreeHeightValue = cutTree(mid);
+	
+		if (getTreeHeightValue < M)
+		{
+			end = mid - 1;
+		}
+		else if (getTreeHeightValue >= M)
+		{
+			start = mid + 1;
+			answer = std::max(answer, mid);
+		}
+	}
+	std::cout << answer << std::endl;
+}
+
+int main()
+{
+	std::cin >> N >> M;
+
+	for (int count = 0; count < N; ++count)
+	{
+		int height;
+		std::cin >> height;
+		treeHeight.emplace_back(height);
+	}
+
+	solution();
+	return EXIT_SUCCESS;
+}
+
---
+++ 백준/boj_2839/boj_2839/boj_2839.cpp
@@ -0,0 +1,30 @@
+﻿// boj_2839.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int dp[50001];
+
+int main()
+{
+	int N;
+	std::cin >> N;
+
+	dp[3] = 1;
+	dp[5] = 1;
+
+
+	for (int n = 6; n <= 5000; ++n)
+	{
+		if (dp[n - 3]) 
+			dp[n] = dp[n - 3] + 1;
+	
+		if (dp[n - 5]) dp[n] = dp[n]? std::min(dp[n], dp[n - 5] + 1) : dp[n - 5] + 1;
+	}
+
+	if (dp[N]) std::cout << dp[N] << std::endl;
+	else std::cout << "-1" << std::endl;
+
+	return EXIT_SUCCESS;
+}
+
